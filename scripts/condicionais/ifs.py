e5c4c5a7078bdb6a7bec77825e7458575312129f
Eduardo Marques <202208702007@alunos.estacio.br>
2024-06-05 10:30:45 -0300

    aprendendo sobre condicionais if

---
+++ scripts/condicionais/ifs.py
@@ -0,0 +1,14 @@
+# if / elif / else
+# se / se não / se não 
+
+# entrace = input('Você quer "entrar" ou "sair"? ')
+entrace = 'sair'
+
+if entrace == 'entrar':
+    print('Você entrou no sistema!')
+elif entrace == 'sair':
+    print('Você saiu do sistema!')
+else:
+    print('Você digitou uma resposta inválida!')
+
+print('Tenha um bom dia!')  
