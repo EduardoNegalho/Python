faf8e6fe0f8f7ba7c91ca982d380122af562117b
Eduardo Marques <202208702007@alunos.estacio.br>
2024-06-05 21:35:02 -0300

    exercicio de condicionais

---
+++ Exercicios/condicionais/main.py
@@ -0,0 +1,11 @@
+primeiro_valor = int(input('Digite um número: '))
+segundo_valor = int(input('Digite outro número: '))
+
+# primeiro_valor_float = int(primeiro_valor)
+# segundo_valor_float = int(segundo_valor)
+
+if primeiro_valor > segundo_valor:
+    print(f'{primeiro_valor=} é maior do que o {segundo_valor=}')
+else:
+    print(f'{segundo_valor=} é maior do que o {primeiro_valor=}')
+
