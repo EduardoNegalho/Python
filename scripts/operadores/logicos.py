1fd9e9010fa1c47de7a997bf5b7e7faa7042bfcf
Eduardo Marques <202208702007@alunos.estacio.br>
2024-06-07 22:45:32 -0300

    aprendendo sobre fatiamento de strings e uso do metodo len()

--- scripts/operadores/logicos.py
+++ scripts/operadores/logicos.py
@@ -1,7 +1,27 @@
-option_entrace = 'entrar'
-password = '123456'
+# option_entrace = 'entrar'
+# password = '123456'
 
-password_user = '123456'
+# password_user = '123456'
 
-if (option_entrace == 'entrar' and password == password_user):
-    print('Entrada confirmada!')
+# if (option_entrace == 'entrar' and password == password_user):
+#     print('Entrada confirmada!')
+
+print('Operador "and"')
+# Só é True quando todas as avaliações forem True
+print(True and True)
+print(True and False)
+print(False and True)
+print(True and 0 and 1) # operação de curto circuito
+
+
+print('\nOperador "or"')
+# Só é True quando pelo menos uma das avaliações forem True
+print(True or True)
+print(True or False)
+print(False or False)
+print(False or 0 or '1') # operação de curto circuito
+
+print('\nOperador "not"')
+# Usado para inverter expressões
+print(not(True and True))
+print(not(False or False))
---
+++ scripts/tiposDeDados/str/main.py
@@ -0,0 +1,20 @@
+var = 'Hello World!'
+
+# fatiamento de str
+# [i:f:p]
+
+# pega do indice até o final
+print(var[6:])
+
+# pega do indice inicial até o indice final exclusivo
+print(var[0:5])
+
+# pega do inicio até o final pulando de 2 em 2
+print(var[0:: 2])
+
+# pega do final até o inicio
+nome = 'eduardo'
+print(nome[::-1])
+
+# tamanho
+print(len(var))
