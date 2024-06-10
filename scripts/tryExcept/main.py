0d0b6f6ac8aec81b6096c372e38b5048f2d32f55
Eduardo Marques <202208702007@alunos.estacio.br>
2024-06-09 21:43:10 -0300

    entendendo o conceito do try except

---
+++ scripts/tryExcept/main.py
@@ -0,0 +1,9 @@
+# try -> tentar executar o código
+# except -> ocorreu algum erro ao tentar executar
+
+try:
+    # tenta executar
+    ...
+except:
+    # executa caso ocorra um erro no try
+    ...
