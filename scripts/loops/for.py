a43aedadd9bbab2ee9f2cca31095a5a841596711
Eduardo Marques <202208702007@alunos.estacio.br>
2024-06-17 08:39:09 -0300

    aprendendo sobre o loop for

---
+++ scripts/loops/for.py
@@ -0,0 +1,16 @@
+word = 'paralelepipedo'
+
+for letter in word:
+    print(letter)
+
+# De 0 a 9
+# range(10)
+
+# De 1 a 10
+# range(1, 11)
+
+# De 1 a 15 de 2 em 2
+# range(1, 15, 2)
+
+for x in range(1, 11):
+    print(x)
