7a4b8bc9a176e6b58db30881d89487eb7cb78e85
Eduardo Marques <202208702007@alunos.estacio.br>
2024-06-17 08:47:07 -0300

    tratando possivel erro quando o usuário escolhe um dificuldade inválida

--- Exercicios/adivinha_numero/main.py
+++ Exercicios/adivinha_numero/main.py
@@ -5,45 +5,49 @@ print('Bem-Vindo ao jogo de Adivinhação de Números')
 print('Acerte o número entre 1 e 100')
 print('-' * 43)
 
-print('Escolha a dificuldade do jogo')
-option = input('(F)Fácil - (M)Médio - (D)Difícil: ').lower()
+while True:
+    print('Escolha a dificuldade do jogo')
+    option = input('(F)Fácil - (M)Médio - (D)Difícil: ').lower()
 
-try:
     if option == 'f':
         chances = 20
+        break
     elif option == 'm':
         chances = 15
+        break
     elif option == 'd':
         chances = 10
+        break
+    else:
+        print('Escolha da dificuldade inválida!\n')
+
+print(f'Você terá {chances} rodadas')
+
+random_number = random.randint(1, 100)
+count = 1
+
+while count <= chances:
+    print(f'\nRodada: {count}')
+    kick = input('Digite seu chute: ')
+
+    try:
+        kick_int = int(kick)
+    except:
+        print('Digite um chute válido')
+        continue
+
+    if kick_int == random_number:
+        print('Parabéns você acertou')
+        break
+
+    if kick_int < random_number:
+        print('Chute baixo')
+    else:
+        print('Chute alto')
+    
+    if count == chances:
+        print('Você perdeu!')
+        break
         
-    print(f'Você terá {chances} rodadas')
+    count += 1
 
-    random_number = random.randint(1, 100)
-    count = 1
-
-    while count <= chances:
-        print(f'\nRodada: {count}')
-        kick = input('Digite seu chute: ')
-
-        try:
-            kick_int = int(kick)
-        except:
-            print('Digite um chute válido')
-            continue
-
-        if kick_int == random_number:
-            print('Parabéns você acertou')
-            break
-
-        if kick_int < random_number:
-            print('Chute baixo')
-        else:
-            print('Chute alto')
-        
-        if count == chances:
-            print('Você perdeu!')
-            break
-            
-        count += 1
-except:
-    print('Escolha da dificuldade inválida!')
