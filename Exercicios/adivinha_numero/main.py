17d117ffa8e5870a23519d8639682c98dd871746
Eduardo Marques <202208702007@alunos.estacio.br>
2024-06-17 08:39:40 -0300

    tratando possivel erro quando usuário escolhe um opção de dificuldade que não existe

--- Exercicios/adivinha_numero/main.py
+++ Exercicios/adivinha_numero/main.py
@@ -8,39 +8,42 @@ print('-' * 43)
 print('Escolha a dificuldade do jogo')
 option = input('(F)Fácil - (M)Médio - (D)Difícil: ').lower()
 
-if option == 'f':
-    chances = 20
-elif option == 'm':
-    chances = 15
-elif option == 'd':
-    chances = 10
-print(f'Você terá {chances} rodadas')
-
-random_number = random.randint(1, 100)
-count = 1
-
-while count <= chances:
-    print(f'\nRodada: {count}')
-    kick = input('Digite seu chute: ')
-
-    try:
-        kick_int = int(kick)
-    except:
-        print('Digite um chute válido')
-        continue
-
-    if kick_int == random_number:
-        print('Parabéns você acertou')
-        break
-
-    if kick_int < random_number:
-        print('Chute baixo')
-    else:
-        print('Chute alto')
-    
-    if count == chances:
-        print('Você perdeu!')
-        break
+try:
+    if option == 'f':
+        chances = 20
+    elif option == 'm':
+        chances = 15
+    elif option == 'd':
+        chances = 10
         
-    count += 1
+    print(f'Você terá {chances} rodadas')
 
+    random_number = random.randint(1, 100)
+    count = 1
+
+    while count <= chances:
+        print(f'\nRodada: {count}')
+        kick = input('Digite seu chute: ')
+
+        try:
+            kick_int = int(kick)
+        except:
+            print('Digite um chute válido')
+            continue
+
+        if kick_int == random_number:
+            print('Parabéns você acertou')
+            break
+
+        if kick_int < random_number:
+            print('Chute baixo')
+        else:
+            print('Chute alto')
+        
+        if count == chances:
+            print('Você perdeu!')
+            break
+            
+        count += 1
+except:
+    print('Escolha da dificuldade inválida!')
